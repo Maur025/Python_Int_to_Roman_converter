dbc386e44985ee75e6bd04c871367840f269bbc3
Mau025 <mmoyapalli@gmail.com>
2020-06-16 16:45:11 -0400

    realice pruebas y agregue un git ignore

--- Prueba1.py
+++ Prueba1.py
@@ -1,1 +1,28 @@
-print("Hola Mundo")
+myString = "Hello World"
+#print(dir(myString))
+
+# print(myString.upper())
+# print(myString.lower())
+# print(myString.capitalize())
+# print(myString.swapcase())
+# print(myString.replace('Hello','Hola').upper()) # remplazar 
+# print(myString.count('l'))  #contar letra
+
+# print(myString.startswith('H')) #buscar si el string empieza con la letra o palabra indicada
+# print(myString.endswith('d'))# busca si el string termina con la letra o palabra indicada
+
+# print(myString.split())
+# print(myString.find('o'))
+
+# print(len(myString)) #longitud del string
+# print(myString.index('e'))
+
+# print(myString.isnumeric())
+# print(myString.isalpha())
+
+# print(myString[4])
+# print(myString[-1])
+
+print('this message says : ' + myString)
+print(f'this message says : {myString}')
+print('this message says : {0}'.format(myString))
